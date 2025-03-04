109613c0018e028023675a864b5bac875f3d624b
JNadori <degututaj@gmail.com>
2025-03-04 09:26:35 +0100

    add get circuit depth to qgd_circuit

--- squander/gates/qgd_Circuit.py
+++ squander/gates/qgd_Circuit.py
@@ -60,6 +60,7 @@ class qgd_Circuit(qgd_Circuit_Wrapper):
 
     def __init__( self, qbit_num ):
 
+        self.qbit_num = qbit_num
         # call the constructor of the wrapper class
         super(qgd_Circuit, self).__init__( qbit_num )
 
@@ -351,6 +352,63 @@ class qgd_Circuit(qgd_Circuit_Wrapper):
                 else:
                     number_of_gates['U3']+=1
         return number_of_gates
+        
+    def get_Circuit_Depth(self):
+        used_gates_idx = []
+        gates = self.get_Gates()
+        depth = 0
+        gate_start_idx = 0
+        gate_groups = []
+        while (len(used_gates_idx) != len(gates)):
+            involved_qbits=[]
+            gate_start_idx_prev = gate_start_idx
+            gate_idx = gate_start_idx
+            depth += 1
+            control_last_single=[False]*self.qbit_num
+            gate_group=[]
+            while((len(involved_qbits)<self.qbit_num) and gate_idx<len(gates)):
+                if gate_idx not in used_gates_idx:
+                    target_qbit = gates[gate_idx].get_Target_Qbit()
+                    control_qbit = gates[gate_idx].get_Control_Qbit()
+                    gate = gates[gate_idx]
+                    if isinstance( gate, CROT ):
+                        if ((control_qbit in involved_qbits) and control_last_single[control_qbit]==False) and (target_qbit not in involved_qbits):
+                            involved_qbits.append(target_qbit)
+                            used_gates_idx.append(gate_idx)
+                            gate_group.append(gate_idx)
+                            control_last_single[control_qbit]=False
+                        elif ((control_qbit in involved_qbits) and control_last_single[control_qbit]==True) and (target_qbit not in involved_qbits):
+                            involved_qbits.append(target_qbit)
+                            if (gate_start_idx == gate_start_idx_prev):
+                                gate_start_idx=gate_idx
+                        elif (control_qbit not in involved_qbits) and (target_qbit not in involved_qbits):
+                            involved_qbits.append(target_qbit)
+                            involved_qbits.append(control_qbit)
+                            used_gates_idx.append(gate_idx)
+                            gate_group.append(gate_idx)
+                            control_last_single[control_qbit]=False
+                        elif (gate_start_idx == gate_start_idx_prev):
+                            gate_start_idx=gate_idx
+                    else:
+                        if control_qbit!=-1:
+                            if (control_qbit not in involved_qbits) and (target_qbit not in involved_qbits):
+                                involved_qbits.append(target_qbit)
+                                involved_qbits.append(control_qbit)
+                                used_gates_idx.append(gate_idx)
+                                gate_group.append(gate_idx)
+                            elif (gate_start_idx == gate_start_idx_prev):
+                                gate_start_idx=gate_idx
+                        else:
+                            if target_qbit not in involved_qbits:
+                                involved_qbits.append(target_qbit)
+                                used_gates_idx.append(gate_idx)
+                                control_last_single[target_qbit]=True
+                                gate_group.append(gate_idx)
+                            elif (gate_start_idx == gate_start_idx_prev):
+                                gate_start_idx=gate_idx
+                gate_idx+=1
+            gate_groups.append(gate_group)
+        return depth
 
 
 #@brief Method to get the list of parent gate indices. Then the parent gates can be obtained from the list of gates involved in the circuit.
